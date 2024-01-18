627bff8ad8dcef14af25d28745a35cd212f0a6b7
juan1david <juancr200502@gmail.com>
2024-01-18 08:59:37 -0500

    subir repositorio

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-const functions = require('./functions.js')
+// Importa el módulo 'functions.js' y asigna sus funciones al objeto 'functions'.
+const functions = require('./functions.js');
 
+// Muestra en la consola el objeto 'functions', que contiene las funciones importadas.
 console.log(functions);
+// Utiliza 'process.stdout.write' para imprimir "Ejercicio" sin cambiar de línea.
 process.stdout.write("Ejercicio\n");
-
-console.log(functions.add(5,3));
-console.log(functions.substract(2,8));
-console.log(functions.multiply(2,8));
-console.log(functions.divide(2,0,9));
+// Llama a la función 'add' del módulo 'functions' con los argumentos 5 y 3, e imprime el resultado.
+console.log(functions.add(5, 3));
+// Llama a la función 'substract' del módulo 'functions' con los argumentos 2 y 8, e imprime el resultado.
+console.log(functions.substract(2, 8));
+// Llama a la función 'multiply' del módulo 'functions' con los argumentos 2 y 8, e imprime el resultado.
+console.log(functions.multiply(2, 8));
+// Llama a la función 'divide' del módulo 'functions' con los argumentos 2, 0 y 9, e imprime el resultado.
+console.log(functions.divide(2, 0, 9));
--- functions.js
+++ functions.js
@@ -1,29 +1,35 @@
+// Crea un objeto vacío llamado 'Math'.
 const Math = {};
 
-function add(a,b) {
+// Define la función 'add' que suma dos números.
+function add(a, b) {
     return a + b;
 }
 
-function substract(a,b) { 
+// Define la función 'substract' que resta el segundo número del primero.
+function substract(a, b) { 
     return a - b;
 }
 
-function multiply(a,b) {
+// Define la función 'multiply' que multiplica dos números.
+function multiply(a, b) {
     return a * b;
 }
 
-function divide(a,b) {
- if (b == 0)  {
-        return "error: no se puede dividir entre cero"
+// Define la función 'divide' que divide el primer número por el segundo, maneja la división por cero.
+function divide(a, b) {
+    if (b == 0)  {
+        return "error: no se puede dividir entre cero";
     } else {
         return a / b;
     }
-
 }
 
+// Asigna las funciones al objeto 'Math'.
 Math.add = add;
 Math.substract = substract;
 Math.multiply = multiply;
 Math.divide = divide;
 
+// Exporta el objeto 'Math' para que esté disponible para su uso en otros módulos.
 module.exports = Math;
